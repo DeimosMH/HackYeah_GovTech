9e6a31372d7795dfe18ab33011d30bbd3bfc0ba8
Maciej Hejlasz <maciejhejlasz@gmail.com>
2023-10-01 08:36:55 +0200

    amend

---
+++ script/chat2.js
@@ -0,0 +1,19 @@
+let currentQAIndex = 0;
+
+function displayNextQA() {
+    let questionElement = document.getElementById('question');  // Element na pytania.
+    let answerElement = document.getElementById('answer');      // Element na odpowiedzi.
+
+    if (currentQAIndex < qAndA.length) {
+        questionElement.innerText = qAndA[currentQAIndex].q;
+        answerElement.innerText = qAndA[currentQAIndex].a;
+        currentQAIndex++;
+        setTimeout(typeWriter, speed);
+    } else {
+        questionElement.innerText = finalResponse;
+        answerElement.innerText = "";
+
+        let btn = document.getElementById('btn');
+        btn.style.display = 'none';
+    }
+}
