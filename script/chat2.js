d1c5d0a04b713923132346c4f8650454f51218a2
Maciej Hejlasz <maciejhejlasz@gmail.com>
2023-10-01 09:25:14 +0200

    working desing

--- script/chat2.js
+++ script/chat2.js
@@ -3,16 +3,18 @@ let currentQAIndex = 0;
 function displayNextQA() {
     let questionElement = document.getElementById('question');  // Element na pytania.
     let answerElement = document.getElementById('answer');      // Element na odpowiedzi.
+    let resultDisplay = document.getElementById('resultdata');
 
     if (currentQAIndex < qAndA.length) {
         questionElement.innerText = qAndA[currentQAIndex].q;
         answerElement.innerText = qAndA[currentQAIndex].a;
         currentQAIndex++;
-        // setTimeout(typeWriter, speed);
+        typeWriter();
     } else {
 
         questionElement.innerText = "Propozycja AI";
         answerElement.innerText = finalResponse;
+        resultDisplay.style.display = "block";
         
         let btn = document.getElementById('btn');
         btn.style.display = 'none';
